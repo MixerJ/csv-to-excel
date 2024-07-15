c6169dac6dd128377a850f43482753d55c15116c
Zhu ShiWen <shiwen@boostvision.com.cn>
2024-07-15 19:38:45 +0800

    feat: modify nextjs to astro

--- next-sitemap.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: 'https://www.csv2xlsx.xyz', // 替换为你的域名
-    generateRobotsTxt: true, // (optional)
-    // ...其他配置
-}
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,11 +2,7 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{js,jsx}',
-    './components/**/*.{js,jsx}',
-    './app/**/*.{js,jsx}',
-    './src/**/*.{js,jsx}',
-  ],
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   prefix: "",
   theme: {
     container: {
